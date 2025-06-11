cd20ac600f5c28f27a340122bff842b499cf96c4
ashish-choudhari-git <contact.ashishchoudhari@gmail.com>
2025-06-11 16:14:50 +0530

    Day 1

---
+++ assets/js/api.js
@@ -0,0 +1,4 @@
+@license MIT
+@fileoverview All api related stuff like api_key, api request etc.
+@copyright ashish-choudhari-git 2025 All rights reserved
+@author ashish-choudhari-git <Contact.ashishchoudhari@gmail.com>
---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+@license MIT
+  @copyright ashish choudhari 2025 All rights reserved
+  @author ashish-choudhari-git <mohammadsadee24@gmail.com>
+ 
