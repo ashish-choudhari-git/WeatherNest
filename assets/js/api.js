3fbb2368042f166a70588628e5c6fe9f732a7d12
ashish-choudhari-git <contact.ashishchoudhari@gmail.com>
2025-08-12 19:28:19 +0530

    Created api javascript code and modules

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,28 @@
-@license MIT
-@fileoverview All api related stuff like api_key, api request etc.
-@copyright ashish-choudhari-git 2025 All rights reserved
-@author ashish-choudhari-git <Contact.ashishchoudhari@gmail.com>
+'se strict';
+
+const api_key = "099344ea2dbed236300c4b12ee35236f";
+// const URL ="https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+
+export const fetchData = function(URL,callback){
+    fetch(`${URL}&apiid=${api_key}`)
+        .then(res=>res.json()).then(data=>callback(data)
+    );
+}
+
+export const url = {
+    currentWeather(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}`
+    },
+    forecast(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}`
+    },
+    airPollution(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat,lon){
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    geo(query){
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
--- assets/js/app.js
+++
@@ -1,4 +0,0 @@
-@license MIT
-  @copyright ashish choudhari 2025 All rights reserved
-  @author ashish-choudhari-git <mohammadsadee24@gmail.com>
- 
